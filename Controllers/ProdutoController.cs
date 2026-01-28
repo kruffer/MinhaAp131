b953239bee24f5f1a8d7368a2e2683c4bc521f52
Kaio Ruffer <kaioruffer25@gmail.com>
2026-01-28 11:27:43 -0300

    Criei API de produtos com GET, POST e DELETE

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,17 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using MinhaApi31.Models;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace MinhaApi31.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    
     public class ProdutoController : ControllerBase
     {
+    private static List<Produto> produtos = new List<Produto>();
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("API .NET core 3.1 funcionando!");
+            return Ok(produtos);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Produto produto)
+        {
+            produto.Id = produtos.Count +1;
+            produtos.Add(produto);
+            return Created("", produto);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var produto = produtos.FirstOrDefault(p => p.Id == id);
+            if (produto == null)
+            return NotFound();
+
+            produtos.Remove(produto); 
+            return NoContent();
         }
     }
 }
